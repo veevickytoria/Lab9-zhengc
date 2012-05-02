18f454e0f32bccdfeaff8bb43e646095f00b7176
zhengc <zhengc@rose-hulman.edu>
2012-05-02 06:44:42 -0400

    War games - two more scenarios passed

--- ChatBot/chatbot.rb
+++ ChatBot/chatbot.rb
@@ -3,4 +3,9 @@ puts 'What is your name?'
 name = gets.chomp
 puts "Hello, #{name}!"
 puts "Pick a game to play:\n1. Sims\n2. Global Thermonuclear War\n3. COD\n4. Angry Bird\n5. Super Mario"
-
+number = gets.chomp
+if number == "2"
+	puts "BOOM!"
+else
+	puts "I refuse to play that game."
+end
