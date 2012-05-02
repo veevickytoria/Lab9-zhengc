86a12fed25a789db5d50a8fd92e300bd4bbdaa0d
zhengc <zhengc@rose-hulman.edu>
2012-05-02 06:40:24 -0400

    War games - first scenario passed

--- ChatBot/chatbot.rb
+++ ChatBot/chatbot.rb
@@ -2,4 +2,5 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts "Hello, #{name}!"
+puts "Pick a game to play:\n1. Sims\n2. Global Thermonuclear War\n3. COD\n4. Angry Bird\n5. Super Mario"
 
